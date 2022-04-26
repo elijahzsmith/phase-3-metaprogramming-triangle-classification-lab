6cc64ec19499a88620d7edd00b211bb5072cfcad
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-25 21:53:52 -0500

    commit tree 28ae9f254db20e2c5af90007328fa11c532b197b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @tri_arr = [@a, @b, @c]
+  end
+
+  def kind
+    triangle_check
+    if @tri_arr.uniq.length == 2 
+    :isosceles 
+    elsif @tri_arr.uniq.length == 3 
+    :scalene
+    elsif @tri_arr.all?
+    :equilateral
+    end
+  end
+
+  def triangle_check
+    if @tri_arr.include?(0) || (@a + @b) <= @c || (@b + @c) <= @a || (@c + @a) <= @b
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "wrong"
+    end
+  end
+
 end
+
